267e1a1588c4ff5cb7af399bea4a66b783d003ee
Sanjay Gidwani <sgidwani59@gmail.com>
2017-08-04 14:48:46 +0530

    Update Depth_First_Search.cpp

--- GraphTheory/Depth_First_Search.cpp
+++ GraphTheory/Depth_First_Search.cpp
@@ -3,30 +3,30 @@
 
 void dfs(vector <int> v[], bool vis[], int s) {
 	vis[s] = 1;
-    	for(int i = 0; i < v[s].size(); i++) {
-    		if(!vis[v[s][i]]) {
+	for(int i = 0; i < v[s].size(); i++) {
+		if(!vis[v[s][i]]) {
 			dfs(v, vis, v[s][i]);
 		}
-    	}
+	}
 }
 
 // path finding using dfs.
 // O(V + E)
 
 void dfs(vector <int> v[], int path[], int path_index, bool vis[], int s, int d) {
-    	vis[s] = 1;
-    	path[path_index] = s;
-    	path_index += 1;
-    	if(s == d) {
+	vis[s] = 1;
+	path[path_index] = s;
+	path_index += 1;
+	if(s == d) {
 		// to do
 		return;
-    	} else {
-        	for(int i = 0; i < v[s].size(); i++) {
-            		if(!vis[v[s][i]]) {
-            			dfs(v, path, path_index, vis, v[s][i], d);
-            		}
-        	}
-    	}
-    	path_index -= 1;
-    	vis[s] = 0;
+	} else {
+		for(int i = 0; i < v[s].size(); i++) {
+			if(!vis[v[s][i]]) {
+				dfs(v, path, path_index, vis, v[s][i], d);
+			}
+		}
+	}
+	path_index -= 1;
+	vis[s] = 0;
 }
