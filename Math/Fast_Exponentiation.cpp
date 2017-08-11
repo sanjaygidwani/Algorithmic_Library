e8d70e3f647226bc768e01d32e9b00ee51e5d778
Sanjay Gidwani <sgidwani59@gmail.com>
2017-08-11 16:17:37 +0530

    Add files via upload

---
+++ Math/Fast_Exponentiation.cpp
@@ -0,0 +1,14 @@
+// (A ^ B) % M
+// O(logN)
+
+long long fast_expo(long long a, long long b, long long md) {
+	long long ans = 1;
+	while(b > 0) {
+		if(b % 2 == 1) {
+			ans = (ans * a) % md;
+		}
+		a = (a * a) % md;
+		b = b / 2;
+	}
+	return ans;
+}
