ccc1ec4c26d9e8dac850b9a9addab5a4b59ec130
Sanjay Gidwani <sgidwani59@gmail.com>
2017-08-27 18:57:11 +0530

    Add files via upload

---
+++ Math/Euler_Totient_Function.cpp
@@ -0,0 +1,45 @@
+// compute etf value
+// O(sqrt(N))
+
+int phi(int n) {
+	int result = n;
+	for(int p = 2; p * p <= n; ++p) {
+		if(n % p == 0) {
+			while(n % p == 0) {
+				n /= p;
+			}
+			result -= result / p;
+		}
+	}
+	if(n > 1) {
+		result -= result / n;
+	}
+	return result;
+}
+
+// precompute etf
+// O(N log(N))
+
+long long etf[N];
+long long prime[N];
+void calc_etf() {
+	etf[1] = 1;
+	for(long long i = 2; i <= N; i++) {
+		if(!prime[i]) {
+			etf[i] = i - 1;
+			for(long long j = 1; j * i <= N; j++) {
+				if(!prime[j * i]) {
+					prime[j * i] = i;
+				}
+			}
+		} else {
+			etf[i] = etf[prime[i]] * etf[i / prime[i]];
+			long long g = 1;
+			if(i % (prime[i] * prime[i]) == 0) {
+				g = prime[i];
+			}
+			etf[i] *= g;
+			etf[i] /= etf[g];
+		}
+	}
+}
