0ac51568df7aec82a40a6e15a9f05a922f295d71
Sanjay Gidwani <sgidwani59@gmail.com>
2017-12-24 13:36:21 +0530

    Update Primality_Test.cpp

--- Math/Primality_Test.cpp
+++ Math/Primality_Test.cpp
@@ -4,7 +4,7 @@ bool isPrime(int N) {
 	if(N == 1)
 		return 0;
 	int cnt = 0;
-	for(int i = 1; i * i <= N; ++i) {
+	for(int i = 1; i * i <= N; ++ i) {
 		if(N % i == 0) {
 			if(i * i == N)
 				cnt += 1;
