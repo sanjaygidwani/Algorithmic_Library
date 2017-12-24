6dbab6a05f344aed30f91b0f8d8c543ac040c164
Sanjay Gidwani <sgidwani59@gmail.com>
2017-12-24 13:34:50 +0530

    Update Primality_Test.cpp

--- Math/Primality_Test.cpp
+++ Math/Primality_Test.cpp
@@ -1,19 +1,18 @@
-// check if the number is prime or not
-// O(sqrt(N))
+// Time Complexity: O(sqrt(N))
 
-bool prm_chk(int N) {
+bool isPrime(int N) {
+	if(N == 1)
+		return 0;
 	int cnt = 0;
 	for(int i = 1; i * i <= N; ++i) {
 		if(N % i == 0) {
-			if(i * i == N) {
+			if(i * i == N)
 				cnt += 1;
-			} else {
+			else
 				cnt += 2;
-			}
 		}
-		if(cnt > 2) {
+		if(cnt > 2)
 			return 0;
-		}
 	}
 	return 1;
 }
