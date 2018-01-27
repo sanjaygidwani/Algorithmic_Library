63775b47df14024459f41b60672e737d662b3702
Sanjay Gidwani <sgidwani59@gmail.com>
2018-01-27 17:24:00 +0530

    Update GCD_And_Modular_Inverse.cpp

--- Math/GCD_And_Modular_Inverse.cpp
+++ Math/GCD_And_Modular_Inverse.cpp
@@ -12,30 +12,30 @@ int gcd(int a, int b) {
 // O(log(max(A, B)))
 
 int d, x, y;
-void extended_euclid(int a, int b) {
+void extendedEuclid(int a, int b) {
 	if(b == 0) {
 		d = a;
 		x = 1;
 		y = 0;
 	} else {
-		extended_euclid(b, a % b);
+		extendedEuclid(b, a % b);
 		int tmp = x;
 		x = y;
 		y = tmp - (a / b) * y;
 	}
 }
 
-// modular inverse when m is prime
-// O(logM)
+// modular inverse, when m is prime
+// O(log M)
 
-int mod_invrs(int a, int m) {
-	return fast_expo(a, m - 2, m);
+int modInverse(int a, int m) {
+	return fastExpo(a, m - 2, m);
 }
 
 // modular inverse
 // O(log(max(A, M)))
 
-int mod_invrs(int a, int m) {
-	extended_euclid(a, m);
+int modInverse(int a, int m) {
+	extendedEuclid(a, m);
 	return (x % m + m) % m;
 }
