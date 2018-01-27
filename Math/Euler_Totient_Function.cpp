edbbb32414e6c1f3c61630f9ae6bebf4dfbdeec9
Sanjay Gidwani <sgidwani59@gmail.com>
2018-01-27 17:27:07 +0530

    Update Euler_Totient_Function.cpp

--- Math/Euler_Totient_Function.cpp
+++ Math/Euler_Totient_Function.cpp
@@ -5,15 +5,13 @@ int phi(int n) {
 	int result = n;
 	for(int p = 2; p * p <= n; ++ p) {
 		if(n % p == 0) {
-			while(n % p == 0) {
+			while(n % p == 0)
 				n /= p;
-			}
 			result -= result / p;
 		}
 	}
-	if(n > 1) {
+	if(n > 1)
 		result -= result / n;
-	}
 	return result;
 }
 
@@ -22,22 +20,20 @@ int phi(int n) {
 
 long long etf[N];
 long long prime[N];
-void calc_etf() {
+void calcETF() {
 	etf[1] = 1;
 	for(long long i = 2; i <= N; i++) {
 		if(!prime[i]) {
 			etf[i] = i - 1;
 			for(long long j = 1; j * i <= N; j++) {
-				if(!prime[j * i]) {
+				if(!prime[j * i])
 					prime[j * i] = i;
-				}
 			}
 		} else {
 			etf[i] = etf[prime[i]] * etf[i / prime[i]];
 			long long g = 1;
-			if(i % (prime[i] * prime[i]) == 0) {
+			if(i % (prime[i] * prime[i]) == 0)
 				g = prime[i];
-			}
 			etf[i] *= g;
 			etf[i] /= etf[g];
 		}
