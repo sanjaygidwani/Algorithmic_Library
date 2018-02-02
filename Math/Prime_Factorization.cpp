c5b2ddfb93bbd87d190dbca65f81784c116a584b
Sanjay Gidwani <sgidwani59@gmail.com>
2018-02-02 23:04:10 +0530

    Update Prime_Factorization.cpp

--- Math/Prime_Factorization.cpp
+++ Math/Prime_Factorization.cpp
@@ -4,27 +4,23 @@
 int spf[N];
 void sieve_prm_fact() {
 	spf[1] = 1;
-	for(int i = 2; i < N; i++) {
+	for(int i = 2; i < N; i++)
 		spf[i] = i;
-	}
-	for(int i = 4; i < N; i += 2) {
+	for(int i = 4; i < N; i += 2)
 		spf[i] = 2;
-	}
 	for(int i = 3; i * i < N; i++) {
 		if(spf[i] == i) {
 			for(int j = i * i; j < N; j += i) {
-				if(spf[j] == j) {
+				if(spf[j] == j)
 					spf[j] = i;
-				}
 			}
 		}
 	}
 }
 
 // O(logN)
- 
-vector <int> factorize(int x) {
-	vector <int> ret;
+vector<int> factorize(int x) {
+	vector<int> ret;
 	while(x != 1) {
 		ret.push_back(spf[x]);
 		x = x / spf[x];
