f3a836f1ddafcf588f03426b88e487dde8473b3b
Sanjay Gidwani <sgidwani59@gmail.com>
2018-06-19 10:34:59 +0530

    Update QuickSort.cpp

--- Algorithms/Sorting/QuickSort.cpp
+++ Algorithms/Sorting/QuickSort.cpp
@@ -4,7 +4,7 @@
 int partition(int a[], int low, int high) {
     int pivot = a[high];
     int i = low - 1;
-    for(int j = 0; j < high; j ++) {
+    for(int j = low; j < high; j ++) {
         if(a[j] <= pivot) {
             i ++;
             swap(a[i], a[j]);
