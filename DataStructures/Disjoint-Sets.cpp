9c3d6f9d7765c6614a8d9e2e6e72537e948a7559
Sanjay Gidwani <sgidwani59@gmail.com>
2019-01-20 16:06:35 +0530

    Update Disjoint-Sets.cpp

--- DataStructures/Disjoint-Sets.cpp
+++ DataStructures/Disjoint-Sets.cpp
@@ -1,41 +1,33 @@
-// union-find algorithm (disjoint sets)
-// O(N logN)
+/**
+ * Time Complexity: O(N * LogN)
+ * Space Complexity: O(N)
+ **/
 
-int par[N];
-int size[N];
+int par[N]; // par[i] = -1, for all i from 1 to N
 
-void init(int n) {
-	for(int i = 1; i <= n; i++) {
-		par[i] = i;
-		size[i] = 1;
-	}
+int root(int x) {
+    int node = x;
+    int parent = par[node];
+    while(parent > 0) {
+        node = parent;
+        parent = par[node];
+    }
+    if(node != x) // collapsing find
+        par[x] = node;
+    return node;
 }
 
-int root(int i) {
-	while(par[i] != i) {
-		par[i] = par[par[i]];
-		i = par[i];
-	}
-	return i;
-}
-
-void weighted_union(int a, int b) {
-	int root_a = root(a);
-	int root_b = root(b);
-	if(root_a == root_b)
-		return;
-	if(size[root_a] < size[root_b]) {
-		par[root_a] = par[root_b];
-		size[root_b] += size[root_a];
-	} 
-	else {
-		par[root_b] = par[root_a];
-		size[root_a] += size[root_b];
-	}
-}
-
-bool find(int a, int b) {
-	if(root(a) == root(b))
-		return 1;
-	return 0;
+void weighted_union(int u, int v) {
+    int ru = root(u);
+    int rv = root(v);
+    if(ru == rv)
+        return;
+    if(abs(par[ru]) < abs(par[rv])) {
+        par[rv] += par[ru];
+        par[ru] = rv;
+    } else {
+        par[ru] += par[rv];
+        par[rv] = ru;
+    }
+    return;
 }
