f7a9c451063cd12f16117d17a2fec09c8990a5e6
Sanjay Gidwani <sgidwani59@gmail.com>
2020-02-16 22:14:38 +0530

    Update Binary_Search.cpp

--- Algorithms/Binary_Search.cpp
+++ Algorithms/Binary_Search.cpp
@@ -1,16 +1,20 @@
-// search for element in the array which is just greater than the given element
-// O(log N)
+/**
+ * Search for an element in the Array which is just greater than the given element
+ * Time Complexity: O(log2 N)
+ */
 
 int binarySearch(int ar[], int n, int element) {
-	int l = 0, r = n;
-	while(l < r) {
-		int mid = (l + r) / 2;
-		if(ar[mid] > element)
-			r = mid;
-		else
-			l = mid + 1;
-	}
-	if(ar[l] > element)
-		return l;
-	return -1;
+    int l = 0, r = n;
+    while(l < r) {
+        int mid = (l + r) / 2;
+        if(ar[mid] > element) {
+            r = mid;
+        } 
+        else {
+            l = mid + 1;
+        }
+    }
+    if(ar[l] > element)
+        return l;
+    return -1;
 }
