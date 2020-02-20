70d25bae8eb647a02140d6f683113ee05b66d9b7
Sanjay Gidwani <sgidwani59@gmail.com>
2020-02-20 20:54:47 +0530

    Update Depth_First_Search.cpp

--- GraphTheory/Depth_First_Search.cpp
+++ GraphTheory/Depth_First_Search.cpp
@@ -1,30 +1,14 @@
-// recursive dfs traversal
-// O(V + E)
+/**
+ * Recursive DFS (Depth First Search) traversal
+ * Time Complexity: O(V + E)
+ */
 
-void dfs(vector <int> v[], bool vis[], int s) {
-	vis[s] = 1;
-	for(int i = 0; i < v[s].size(); i++) {
-		if(!vis[v[s][i]])
-			dfs(v, vis, v[s][i]);
-	}
-}
-
-// path finding using dfs
-// O(V + E)
-
-void dfs(vector <int> v[], int path[], int path_index, bool vis[], int s, int d) {
-	vis[s] = 1;
-	path[path_index] = s;
-	path_index += 1;
-	if(s == d) {
-		// to do
-		return;
-	} else {
-		for(int i = 0; i < v[s].size(); i++) {
-			if(!vis[v[s][i]])
-				dfs(v, path, path_index, vis, v[s][i], d);
-		}
-	}
-	path_index -= 1;
-	vis[s] = 0;
+void dfs(vector<int> graph[], bool vis[], int src) {
+    vis[src] = 1;
+    for(int i = 0; i < graph[src].size(); i ++) {
+        if(!vis[graph[src][i]]) {
+            dfs(graph, vis, graph[src][i]);
+        }
+    }
+    return;
 }
