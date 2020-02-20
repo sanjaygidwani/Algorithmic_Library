4c0866f5a7ea3485f98de8ca9a76849a2599949a
Sanjay Gidwani <sgidwani59@gmail.com>
2020-02-20 20:49:21 +0530

    Update Breadth_First_Search.cpp

--- GraphTheory/Breadth_First_Search.cpp
+++ GraphTheory/Breadth_First_Search.cpp
@@ -1,18 +1,23 @@
-// bfs traversal
-// O(V + E)
-
-void bfs(int s) {
-	queue <int> q;
-	q.push(s);
-	vis[s] = 1;
-	while(!q.empty()) {
-		int p = q.front();
-		q.pop();
-		for(int i = 0; i < v[p].size(); i++) {
-			if(!vis[v[p][i]]) {
- 				q.push(v[p][i]);
-				vis[v[p][i]] = 1;
-			}
-		}
-	}
+/**
+ * BFS (Breadth First Search) traversal
+ * Time Complexity: O(V + E)
+ *
+ * Global declarations: vector<bool> vis(V, 0), vector<int> graph[V]
+ */
+ 
+void bfs(int src) {
+    queue <int> q;
+    q.push(src);
+    vis[src] = 1;
+    while(!q.empty()) {
+        int p = q.front();
+        q.pop();
+        for(int i = 0; i < graph[p].size(); i ++) {
+            if(!vis[graph[p][i]]) {
+                q.push(graph[p][i]);
+                vis[graph[p][i]] = 1;
+            }
+        }
+    }
+    return;
 }
